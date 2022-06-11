3526d7bb934b67f054a76b5ab0324d93f30fbb43
Sardonyx <sardonyx@sardonyx.me>
2022-06-11 22:33:21 +0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const colors = ["#3742fa", "#2ed573", "#ff6348", "#ff4757"]
+const randomColor = colors[Math.floor(Math.random() * colors.length)]
+
+const el = document.createElement("style")
+el.innerHTML = `
+     :root {
+          --accent-color: ${randomColor}
+     }
+`
+
+document.body.append(el)
+
+ScrollReveal({
+     delay: 1.5,
+     scale: .9
+}).reveal(".divided-list-obj");
+
+
+ScrollReveal({
+     delay: 1.5,
+     scale: .7
+}).reveal(".intro-anim");
+
+document.querySelectorAll(".tag[auto-color]").forEach(x => {
+     const color = x.attributes.getNamedItem("auto-color").value
+     x.style.color = color
+     x.style.background = color + "1A"
+})
+
+ScrollReveal({
+     delay: 1.5,
+     scale: .7
+}).reveal(".article");
